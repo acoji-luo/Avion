f181dda4178d80d24eea4e5372617ede615f63b8
acoji-luo <acoji.gm@gmail.com>
2024-08-01 17:23:12 +0300

    add product-info section

---
+++ js/stepper.js
@@ -0,0 +1,52 @@
+const stepperEl = document.querySelector(".stepper");
+
+if (stepperEl) {
+	const stepperInputEl = stepperEl.querySelector(".stepper__input");
+	const stepperBtnMinusEl = stepperEl.querySelector(".stepper__btn--minus");
+	const stepperBtnPlusEl = stepperEl.querySelector(".stepper__btn--plus");
+
+	const stepperMin = Number(stepperInputEl.getAttribute("min"));
+	const stepperMax = Number(stepperInputEl.getAttribute("max"));
+
+	let count = Number(stepperInputEl.value);
+
+	stepperInputEl.addEventListener("change", () => {
+		stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
+		stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
+		if (stepperInputEl.value < stepperMin) {
+			stepperInputEl.value = stepperMin;
+			stepperBtnMinusEl.classList.add("stepper__btn--disabled");
+		}
+
+		if (stepperInputEl.value > stepperMax) {
+			stepperInputEl.value = stepperMax;
+			stepperBtnPlusEl.classList.add("stepper__btn--disabled");
+		}
+	});
+
+	stepperBtnPlusEl.addEventListener("click", () => {
+		count = Number(stepperInputEl.value);
+		if (count < stepperMax) {
+			stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
+			stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
+			count++;
+			stepperInputEl.value = count;
+		}
+		if (count === stepperMax) {
+			stepperBtnPlusEl.classList.add("stepper__btn--disabled");
+		}
+	});
+
+	stepperBtnMinusEl.addEventListener("click", () => {
+		count = Number(stepperInputEl.value);
+		if (count > stepperMin) {
+			stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
+			stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
+			count--;
+			stepperInputEl.value = count;
+		}
+		if (count === stepperMin) {
+			stepperBtnMinusEl.classList.add("stepper__btn--disabled");
+		}
+	});
+}
